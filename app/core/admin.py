c745cb5d11e1981136be0c58cbfbfb36fc010bf3
nhieckqo <nhieckqo9@yahoo.com>
2021-01-17 15:45:06 +0800

    Cofingured progress db

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,21 +6,29 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
-    ordering = ['id']
+    ordering = ['id', ]
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name',)}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-        (_('Important dates'), {'fields', ('last_login',)})
-    )
-    add_fieldsets = (
-        (None,
+        (
+            _('Permissions'),
             {
-                'classes': ('wide',),
-                'fields': ('email', 'password1', 'password2')
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
             }
         ),
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -8,14 +8,14 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@admin.com'
+            email='admin@admin.com',
             password='nimda'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='test@admin.com'
-            password='asdf123'
-            name='TEST USER NAME'
+            email='test@admin.com',
+            password='asdf123',
+            name='TEST USER NAME',
         )
 
     def test_users_listed(self):
@@ -25,13 +25,6 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-
-    def test_user_change_page(self):
-        """Test that the user edit page works"""
-        url = reverse('admin:core_user_change', args=[self.user.id])
-        res = self.client.get(url)
-
-        self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
         """Test that the create user page works"""
